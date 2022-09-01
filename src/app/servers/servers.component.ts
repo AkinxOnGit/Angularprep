9a25f52af55d03d7261ca214b34c10db46caa5d4
Mustafa Akin <mustafa.akin@twinformatics.at>
2022-09-01 16:13:15 +0200

    EP-31: Bindable Properties and Events

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = "No server has yet been added"
+  serverName = ""
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    },2000)
+  }
 
   ngOnInit(): void {
+  }
+
+  onCreateServer(){
+  this.serverCreationStatus = "Server was successfully added!"
+  }
+
+  onUpdateName(event: Event){
+  this.serverName = (<HTMLInputElement> event.target).value
   }
 
 }
